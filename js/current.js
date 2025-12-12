cd9d3f7a6e880450654bce155d852749593357f4
Claire <cameron.liam.yon@gmail.com>
2025-12-12 18:58:45 -0500

    javascript implimented to change link color, support sheet done

---
+++ js/current.js
@@ -0,0 +1,15 @@
+// Changes the color of the link to the page that the user is currently on | Partially copied from Lab E
+const pageURL = window.location.href;
+
+const links = document.querySelectorAll("header nav > ul > li > a");
+
+let linkUrl;
+
+links.forEach((element) => {
+
+    linkUrl = element.href;
+    if(linkUrl == pageURL)
+    {
+        element.classList.add("current");
+    }
+});
